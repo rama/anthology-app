59872d8ce11248b626a2de86706b050d63e2e8b6
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2025-01-17 04:33:48 -0500

    add button component and sign up button

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,6 @@
 import { Bricolage_Grotesque } from "next/font/google";
 import styles from "./page.module.css";
+import Button from "./components/Button";
 
 const appNameFont = Bricolage_Grotesque({
 	weight: "800",
@@ -12,6 +13,7 @@ export default function Home() {
 			<div className={styles.coverTitleGroup}>
 				<h1 className={appNameFont.className}>Anthology</h1>
 				<p>Find your next short story</p>
+				<Button>Sign Up</Button>
 			</div>
 		</div>
 	);
