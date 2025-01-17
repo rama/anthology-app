96f3692284735ebbe17e94264a6289c4acbd0ecd
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2025-01-17 08:47:45 -0500

    add links and styles

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,6 +1,7 @@
 import { Bricolage_Grotesque } from "next/font/google";
 import styles from "./page.module.css";
 import Button from "./components/Button";
+import Link from "next/link";
 
 const appNameFont = Bricolage_Grotesque({
 	weight: "800",
@@ -14,7 +15,12 @@ export default function Home() {
 				<div className={styles.coverTitleGroup}>
 					<h1 className={appNameFont.className}>Anthology</h1>
 					<p>Find your next short story</p>
-					<Button>Sign Up</Button>
+					<Button>
+						<Link href="/sign-up">Sign Up</Link>
+					</Button>
+					<Link className={styles.signInLink} href="/sign-in">
+						Sign In
+					</Link>
 				</div>
 			</div>
 			<div className={styles.visualInterest}></div>
