df4723c2162b3d47fd1b6e1958d5d5080b822ca7
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2025-04-08 15:25:49 -0400

    add under development banner

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,13 @@
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { Bricolage_Grotesque, Merriweather, Oswald } from "next/font/google";
 import theme from "../theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import {
+	CssBaseline,
+	ThemeProvider,
+	AppBar,
+	Typography,
+	Link,
+} from "@mui/material";
 
 const appNameFont = Bricolage_Grotesque({
 	weight: "800",
@@ -38,6 +44,31 @@ export default function RootLayout({ children }) {
 				<AppRouterCacheProvider>
 					<ThemeProvider theme={theme}>
 						<CssBaseline />
+						<AppBar
+							color="secondary"
+							sx={{
+								p: 2,
+							}}
+						>
+							<Typography
+								variant="body1"
+								style={{ flexGrow: 1, textAlign: "center" }}
+							>
+								Hey there! Thanks for stopping by. This project
+								is under active development, so things might
+								change frequently. If you spot any errors or
+								data loss, feel free to{" "}
+								<Link
+									href="https://github.com/rama/anthology-app/issues"
+									sx={{
+										color: "#000",
+										textDecorationColor: "#000",
+									}}
+								>
+									create a GitHub issue.
+								</Link>
+							</Typography>
+						</AppBar>
 						<main>{children}</main>
 					</ThemeProvider>
 				</AppRouterCacheProvider>
